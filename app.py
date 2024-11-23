07192e5ac431955f414539858ad4b1d8df436164
developerfaizaan <155816106+developerfaizaan@users.noreply.github.com>
2024-11-23 11:15:14 +0530

    app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ st.markdown("""
 # Adding a top bar with Login option
 st.markdown("""
     <div class="top-bar">
-        <a href="/login" target="_self">🔒 Login</a>
+        <a href="https://detectifilogin.netlify.app/" target="_self">🔒 Login</a>
     </div>
 """, unsafe_allow_html=True)
 
